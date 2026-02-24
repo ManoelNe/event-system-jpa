3757e8ed48aefc298b211f85dfa3082610af4f24
ManoelNe <manoel_neto@outlook.com>
2026-02-24 06:10:45 -0300

    feat: criação de conexao com h2

--- src/main/java/com/devsuperior/evento/EventoApplication.java
+++ src/main/java/com/devsuperior/evento/EventoApplication.java
@@ -1,13 +1,18 @@
 package com.devsuperior.evento;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EventoApplication {
+public class EventoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
